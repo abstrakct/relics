f783bf48d278fefb1d8ad46435a8f65df3160fe4
Rolf K <abstractstar@gmail.com>
2025-04-30 12:53:37 +0200

    Set Wall as default tile type

--- src/map/tile.rs
+++ src/map/tile.rs
@@ -3,6 +3,6 @@ use serde::{Deserialize, Serialize};
 #[derive(Default, PartialEq, Eq, Hash, Clone, Copy, Deserialize, Serialize)]
 pub enum TileType {
     #[default]
-    Floor,
     Wall,
+    Floor,
 }
