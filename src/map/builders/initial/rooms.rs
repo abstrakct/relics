a68680e8536b254376f48f564fe89360af7b2c57
Rolf K <abstractstar@gmail.com>
2025-04-30 12:57:30 +0200

    Tweak room generator algorithm

--- src/map/builders/initial/rooms.rs
+++ src/map/builders/initial/rooms.rs
@@ -16,13 +16,15 @@ impl RoomsBuilder {
 
     fn build_map(&mut self, build_data: &mut BuilderMap) {
         const MAX_ROOMS: i32 = 30;
-        const MIN_SIZE: i32 = 6;
-        const MAX_SIZE: i32 = 12;
+        const MIN_W: i32 = 3;
+        const MAX_W: i32 = 14;
+        const MIN_H: i32 = 3;
+        const MAX_H: i32 = 10;
         let mut rooms: Vec<MapRect> = Vec::new();
 
         for _i in 0..MAX_ROOMS {
-            let w = rng::range(MIN_SIZE, MAX_SIZE);
-            let h = rng::range(MIN_SIZE, MAX_SIZE);
+            let w = rng::range(MIN_W, MAX_W);
+            let h = rng::range(MIN_H, MAX_H);
             let x = rng::roll_dice(1, build_data.map.width as i32 - w - 1) - 1;
             let y = rng::roll_dice(1, build_data.map.height as i32 - h - 1) - 1;
             let new_room = MapRect::new(x, y, w, h);
