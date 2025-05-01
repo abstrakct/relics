2d6bb0cbf134213fcbba8d1643d855a910366d86
Rolf K <abstractstar@gmail.com>
2025-05-01 14:30:38 +0200

    Fix empty room builder

--- src/map/builders/initial/empty_map.rs
+++ src/map/builders/initial/empty_map.rs
@@ -16,7 +16,7 @@ impl EmptyMapBuilder {
 
     fn build_map(&mut self, build_data: &mut BuilderMap) {
         // Create one big room, leave a 1-tile border
-        let rooms: Vec<MapRect> = vec![MapRect::new(1, 1, build_data.width as i32 - 2, build_data.height as i32 - 2)];
+        let rooms: Vec<MapRect> = vec![MapRect::new(0, 0, build_data.width as i32 - 1, build_data.height as i32 - 1)];
         build_data.rooms = Some(rooms);
     }
 }
