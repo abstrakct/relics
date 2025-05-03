ef8b81667f212e58cbd42b177eaeec130facad8a
Rolf K <abstractstar@gmail.com>
2025-05-03 17:38:38 +0200

    Revert "Screw up"
    This reverts commit 7caae9865b760aa04afa69806bccea6d39197dd9.

--- src/game_event.rs
+++ src/game_event.rs
@@ -1,17 +1,14 @@
 use bevy_ecs::event::Event;
-use serde::{
-    // de::{self, Deserializer, Visitor},
-    Deserialize,
-    Serialize,
-};
+use serde::{Deserialize, Serialize, de::DeserializeOwned};
+use serde_json::Value;
 use strum::Display;
 
 /// Trait for types that can be converted to and from events
-pub trait EventConvertible: Clone + Send + Sync + Default + 'static {
-    /// Convert from a string representation (used in config files)
-    fn from_str(s: &str) -> Option<Self>;
-    /// Convert to a string representation (used in config files)
-    fn to_string(&self) -> String;
+pub trait EventConvertible: Clone + Send + Sync + Default + DeserializeOwned + 'static {
+    /// Convert from a JSON value (used in config files)
+    fn from_value(value: Value) -> Option<Self>;
+    /// Convert to a JSON value (used in config files)
+    fn to_value(&self) -> Value;
 }
 
 #[derive(Event, Debug, Clone, PartialEq, Eq, Serialize, Display, Deserialize)]
@@ -37,27 +34,50 @@ impl Default for GameEvent {
 }
 
 impl EventConvertible for GameEvent {
-    fn from_str(s: &str) -> Option<Self> {
-        match s {
-            "Quit" => Some(GameEvent::Quit),
-            "NextMenuItem" => Some(GameEvent::NextMenuItem),
-            "PrevMenuItem" => Some(GameEvent::PrevMenuItem),
-            "SelectMenuItem" => Some(GameEvent::SelectMenuItem),
-            "GenerateWorld" => Some(GameEvent::GenerateWorld),
-            "StartNewGame" => Some(GameEvent::StartNewGame),
+    fn from_value(value: Value) -> Option<Self> {
+        match value {
+            Value::String(s) => match s.as_str() {
+                "Quit" => Some(GameEvent::Quit),
+                "NextMenuItem" => Some(GameEvent::NextMenuItem),
+                "PrevMenuItem" => Some(GameEvent::PrevMenuItem),
+                "SelectMenuItem" => Some(GameEvent::SelectMenuItem),
+                "GenerateWorld" => Some(GameEvent::GenerateWorld),
+                "StartNewGame" => Some(GameEvent::StartNewGame),
+                _ => None,
+            },
+            Value::Object(obj) => {
+                if let Some(player_move) = obj.get("PlayerMove") {
+                    if let Some(x) = player_move.get("x").and_then(|v| v.as_i64()) {
+                        if let Some(y) = player_move.get("y").and_then(|v| v.as_i64()) {
+                            return Some(GameEvent::PlayerMove {
+                                x: x as i32,
+                                y: y as i32,
+                            });
+                        }
+                    }
+                }
+                None
+            }
             _ => None,
         }
     }
 
-    fn to_string(&self) -> String {
+    fn to_value(&self) -> Value {
         match self {
-            GameEvent::Quit => "Quit".to_string(),
-            GameEvent::NextMenuItem => "NextMenuItem".to_string(),
-            GameEvent::PrevMenuItem => "PrevMenuItem".to_string(),
-            GameEvent::SelectMenuItem => "SelectMenuItem".to_string(),
-            GameEvent::GenerateWorld => "GenerateWorld".to_string(),
-            GameEvent::StartNewGame => "StartNewGame".to_string(),
-            GameEvent::PlayerMove { x, y } => format!("PlayerMove {{ x: {}, y: {} }}", x, y),
+            GameEvent::Quit => Value::String("Quit".to_string()),
+            GameEvent::NextMenuItem => Value::String("NextMenuItem".to_string()),
+            GameEvent::PrevMenuItem => Value::String("PrevMenuItem".to_string()),
+            GameEvent::SelectMenuItem => Value::String("SelectMenuItem".to_string()),
+            GameEvent::GenerateWorld => Value::String("GenerateWorld".to_string()),
+            GameEvent::StartNewGame => Value::String("StartNewGame".to_string()),
+            GameEvent::PlayerMove { x, y } => {
+                let mut obj = serde_json::Map::new();
+                let mut move_obj = serde_json::Map::new();
+                move_obj.insert("x".to_string(), Value::Number((*x).into()));
+                move_obj.insert("y".to_string(), Value::Number((*y).into()));
+                obj.insert("PlayerMove".to_string(), Value::Object(move_obj));
+                Value::Object(obj)
+            }
         }
     }
 }
