0d6b15c5e2ec3cf89de6c25b769c3ee42176a48a
Rolf K <abstractstar@gmail.com>
2025-05-04 10:10:30 +0200

    Add Pythagoras distance calculation

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::{Entity, World, debug};
+use std::cmp::{max, min};
 
 pub fn print_entity_components(world: &World, entity: Entity) {
     debug!("Components for entity {:?}:", entity);
@@ -12,4 +13,15 @@ pub fn print_entity_components(world: &World, entity: Entity) {
             debug!("- {}", type_info.name());
         }
     }
+}
+
+// Distance algorithms
+// Based on / inspired by https://github.com/amethyst/bracket-lib/blob/master/bracket-geometry/src/distance.rs
+
+/// Calculates a Pythagoras distance between two points.
+pub fn distance2d_pythagoras(start: (i32, i32), end: (i32, i32)) -> f32 {
+    let dx = (max(start.0, end.0) - min(start.0, end.0)) as f32;
+    let dy = (max(start.1, end.1) - min(start.1, end.1)) as f32;
+    let dsq = (dx * dx) + (dy * dy);
+    f32::sqrt(dsq)
 }
