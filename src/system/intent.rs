893e1735b14d0f8f32a2fa1c2986b552e8ec1da0
Rolf K <abstractstar@gmail.com>
2025-05-14 19:43:16 +0200

    Better query in intent_system

--- src/system/intent.rs
+++ src/system/intent.rs
@@ -1,6 +1,6 @@
 use crate::{
     CurrentGameData,
-    component::Intent,
+    component::{Intent, Player},
     event::{PlayerMoveRelativeEvent, PlayerSpentEnergy},
 };
 use bevy::prelude::*;
@@ -10,18 +10,18 @@ pub fn intent_system(
     cgd: Res<CurrentGameData>,
     mut move_queue: EventWriter<PlayerMoveRelativeEvent>,
     mut energy_queue: EventWriter<PlayerSpentEnergy>,
-    query: Query<(Entity, &Intent)>,
+    query: Query<(&Intent, Option<&Player>)>,
 ) {
-    for (entity, intent) in query {
+    for (intent, player) in query {
         let base_energy_cost = intent.energy_cost();
         debug!(
-            "entity {} has intent {:?} - it has base cost of {} energy",
-            entity, intent, base_energy_cost
+            "entity has intent {:?} - it has base cost of {} energy",
+            intent, base_energy_cost
         );
 
         match *intent {
             Intent::MoveRelative { dx, dy } => {
-                if entity == cgd.player.unwrap() {
+                if let Some(_player) = player {
                     if cgd.maps.map[cgd.player_pos.map].is_walkable(cgd.player_pos.x + dx, cgd.player_pos.y + dy) {
                         debug_once!("entity is player, sending PlayerMoveRelativeEvent");
                         move_queue.write(PlayerMoveRelativeEvent { dx, dy });
